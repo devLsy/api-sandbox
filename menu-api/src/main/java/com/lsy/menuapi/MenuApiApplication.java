55d8e171f76758f8edc3f57ad86b7532867406bb
이상엽 <infightersy@naver.com>
2025-06-22 07:23:53 +0900

    menu-api 모듈 추가, 루트 그레들 설정에서 모듈 그룹화

--- menu-api/src/main/java/com/lsy/menuapi/MenuApiApplication.java
+++ menu-api/src/main/java/com/lsy/menuapi/MenuApiApplication.java
@@ -1,13 +1,13 @@
-package com.lsy.sandboxapiver1;
+package com.lsy.menuapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SandboxApiVer1Application {
+public class MenuApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SandboxApiVer1Application.class, args);
+        SpringApplication.run(MenuApiApplication.class, args);
     }
 
 }
