09f063e889f658527a3abf92a4956a0a8af3a3c6
이상엽 <infightersy@naver.com>
2025-06-21 12:23:34 +0900

    first commit

---
+++ src/main/java/com/lsy/sandboxapiver1/SandboxApiVer1Application.java
@@ -0,0 +1,13 @@
+package com.lsy.sandboxapiver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxApiVer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxApiVer1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lsy/sandboxapiver1/SandboxApiVer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsy.sandboxapiver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxApiVer1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
