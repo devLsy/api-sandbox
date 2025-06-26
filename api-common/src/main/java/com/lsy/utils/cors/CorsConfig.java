a19580aacfedceb5bc064d4c267d47d73853f89a
이상엽 <infightersy@naver.com>
2025-06-26 22:01:34 +0900

    feat: application.yml의 cors 설정값을 주입받아 CORS 정책 적용

--- api-common/src/main/java/com/lsy/utils/cors/CorsConfig.java
+++ api-common/src/main/java/com/lsy/utils/cors/CorsConfig.java
@@ -1,27 +1,39 @@
 package com.lsy.utils.cors;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+// CORS 허용 설정 클래스 (WebMvcConfigurer 구현)
 @Configuration
-// cros 허용 설정
+@EnableConfigurationProperties(CorsConfig.CorsProperties.class) // CorsProperties를 설정 프로퍼티로 등록
+@RequiredArgsConstructor
 public class CorsConfig implements WebMvcConfigurer {
+
+    private final CorsProperties corsProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                // 허용할 origin(우선은 기존 소스와 동일하게 적용했음)
-                .allowedOrigins(
-                "https://localhost:3005",
-                "https://localhost:3006",
-                "https://kepass-admin-dev.koreanair.com",    // admin dev
-                "https://apidev.koreanair.com",              // apigee dev
-                "https://kepass-admin.koreanair.com",       // admin prd
-                "https://api.koreanair.com"                 // apigee prd
-                )
+                // application.yml 또는 application-<profile>.yml에 정의된
+                // cors.allowed-origins 목록을 프로파일에 따라 자동으로 바인딩하여 사용
+                .allowedOrigins(corsProperties.allowedOrigins.toArray(new String[0]))
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    // application.yml의 cors 프로퍼티 바인딩용 클래스
+    @ConfigurationProperties(prefix = "cors")
+    @Getter @Setter
+    public static class CorsProperties {
+        private List<String> allowedOrigins;    // 허용할 CORS origin 목록
     }
 }
--- menu-api/src/main/java/com/lsy/menuapi/MenuApiApplication.java
+++ menu-api/src/main/java/com/lsy/menuapi/MenuApiApplication.java
@@ -3,7 +3,8 @@ package com.lsy.menuapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// 공통 유틸 패키지를 스캔대상에 추가
+@SpringBootApplication(scanBasePackages = {"com.lsy.menuapi", "com.lsy.utils"})
 public class MenuApiApplication {
 
     public static void main(String[] args) {
